4482606e004ccd362663c820bcba08dbbd27c2a2
yuchaoY <yuanyuchaodashuaige@outlook.com>
2019-12-23 22:38:25 +0800

    add time consuming and roc curve

--- eval.py
+++ eval.py
@@ -1,23 +1,28 @@
 import torch
 import numpy as np
 
-from utils import load_standard_mat, data_generator_mask
+from utils import load_standard_mat, data_generator_mask, plot_roc_curve
 from Dataset import Pixel
 from model import CNND
 
-import sklearn.metrics as metrics
+import time
+import os
 
 # Device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Path setting
+data_root = 'input/'
+data_name = 'AVIRIS-II'
+output_path = 'result/'
+data_path = data_root + data_name + '.mat'
+os.makedirs(output_path, exist_ok=True)
 
 # Hyperparameters
 inner_radius = 3
 outer_radius = 5
 
 # Data generator
-data_root = 'input/'
-data_name = 'AVIRIS-I.mat'
-data_path = data_root + data_name
 hyperdata, gt = load_standard_mat(data_path, gt=True)
 H, W = gt.shape
 dataset = data_generator_mask(hyperdata, inner_radius, outer_radius)
@@ -29,10 +34,13 @@ data_loader = torch.utils.data.DataLoader(dataset=data,
                                            batch_size=1,
                                            shuffle=False)
 
+labels_pred = np.loadtxt(data_name+'.csv', delimiter=',')
+
 # model definition
 model = CNND().to(device)
-model.load_state_dict(torch.load('model.ckpt', map_location=torch.device('cpu')))
+model.load_state_dict(torch.load('model.ckpt', map_location=device))
 
+start = time.time()
 # eval model
 model.eval()
 anomal_detector = []
@@ -48,12 +56,12 @@ with torch.no_grad():
         anomal_detector.append(predicted)
 
 anomal_detector = np.array(anomal_detector)
+end = time.time()
+print('running time:', end - start)
 
-fpr, tpr, threshold = metrics.roc_curve(gt, anomal_detector)
-roc_auc = metrics.auc(fpr, tpr)
-print('roc_auc:', roc_auc)
-
+plot_roc_curve(gt, anomal_detector, data_name)
 anomal_detector = np.reshape(anomal_detector, (H, W))
-np.savetxt('./result/'+data_name + '.csv', anomal_detector, delimiter=',', fmt='%.2f')
+
+np.savetxt('./result/'+data_name + '.csv', anomal_detector, delimiter=',')
 
 
